c655e3cc23b01f6992a476fca172ec54aa9732b8
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-03-17 20:51:17 +0000

    Add number buttons

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import './App.css';
-import CalcButton from "./CalcButton.js";
+import {Buttons} from './Buttons.js'
+
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      output:[]
+    }
   }
 
   render() {
     return (
       <div>
-        <CalcButton buttonName={"button"} />
+        {Buttons.map((values, i) => {
+            return (
+              <div key={i}>
+                {values.map((val) => {
+                  return (
+                    <button key={val.name} value={val.name}>
+                      {val.name}
+                    </button>
+                  );
+                })}
+              </div>
+            );
+          })}
       </div>
     );
   }
